fbc0fcd492b0dcd449172523c3d5ef326b1f9aa3
Chris <obromois@gmail.com>
2014-09-11 09:37:46 +1000

    now ATP

--- spec/controllers/access_test_controller_spec.rb
+++ spec/controllers/access_test_controller_spec.rb
@@ -5,26 +5,12 @@ describe AccessTestController do
   describe "block access when needed" do
     include TestAccess
     actions=[:edit,:update]
-    it_should_route_to('signin_path',actions)
+    it_should_route_to('access_test_index_path',actions)
   end
 
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
       response.should be_success
     end
   end
--- spec/dummy/app/controllers/access_test_controller.rb
+++ spec/dummy/app/controllers/access_test_controller.rb
@@ -4,8 +4,10 @@ class AccessTestController < ApplicationController
   end
 
   def edit
+  	redirect_to access_test_index_path
   end
 
   def update
+  	redirect_to access_test_index_path
   end
 end
