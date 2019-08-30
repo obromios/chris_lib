dde1316a1e3a40291ea2719c9389500f8ee2e26c
obromios <obromios@gmail.com>
2019-08-30 17:17:09 +1000

    Build v1.1.4 of chris_lib

--- script/push_tag.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-# pushes tag to travis so it pushes new
-# gem to rubytems
-require '/Users/Chris/Sites/chris_lib/lib/chris_lib/version.rb'
-include ChrisLib
-`git tag v#{ChrisLib::VERSION} -a`
-`git push origin v#{ChrisLib::VERSION}`
-puts "pushed v#{ChrisLib::VERSION} tag"
