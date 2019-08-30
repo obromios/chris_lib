c1f3a05f5b084f6fec53ae2b8c5bfa3ea83cf194
obromios <obromios@gmail.com>
2019-08-31 09:48:47 +1000

    Reverted to manual deploy of rubygems

--- script/push_gem.rb
+++ script/push_gem.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 require '/Users/Chris/Sites/chris_lib/lib/chris_lib/version.rb'
 include ChrisLib
+msg = "Build v#{ChrisLib::VERSION} of chris_lib"
 `gem build chris_lib`
 `git add .`
-`git commit -m "Build v#{ChrisLib::VERSION} of chris_lib"`
+`git commit -m msg`
 puts "Build v#{ChrisLib::VERSION} of chris_lib"
 `git push origin master`
 `gem push chris_lib-#{ChrisLib::VERSION}.gem`
 puts "pushed v#{ChrisLib::VERSION} to rubygems and github"
-`git tag v#{ChrisLib::VERSION}`
-`git push origin v#{ChrisLib::VERSION}`
+`git tag v#{ChrisLib::VERSION} -m msg`
+`git push --tags`
 puts "pushed v#{ChrisLib::VERSION} tag"
