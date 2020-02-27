fb11bbb3f54ee47555a267b878702cf0dda15343
Chris Drane <obromois@gmail.com>
2020-02-27 17:59:09 +1100

    Build v1.1.7 of chris_lib

--- script/push_gem.rb
+++ script/push_gem.rb
@@ -2,14 +2,15 @@
 require '/Users/Chris/Sites/chris_lib/lib/chris_lib/version.rb'
 include ChrisLib
 msg = "Build v#{ChrisLib::VERSION} of chris_lib"
-
+puts msg
 `gem build chris_lib`
 `git add .`
 `git commit -m "#{msg}"`
 `git push origin master`
-puts msg
-`gem push chris_lib-#{ChrisLib::VERSION}.gem`
+puts 'Push to rubygems, need to enter OTP from Google Authenticator'
+system "gem push chris_lib-#{ChrisLib::VERSION}.gem"
 puts "pushed v#{ChrisLib::VERSION} to rubygems and github"
 `git tag v#{ChrisLib::VERSION} -m msg`
 `git push --tags`
 puts "pushed v#{ChrisLib::VERSION} tag"
+puts 'Bump version, update changelog with git_sha'
