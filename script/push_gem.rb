478edcf7dcb965a95f6eb73385c3183815b7681e
Chris Drane <obromois@gmail.com>
2020-02-27 17:49:46 +1100

    Build v1.1.6 of chris_lib

--- script/push_gem.rb
+++ script/push_gem.rb
@@ -2,12 +2,12 @@
 require '/Users/Chris/Sites/chris_lib/lib/chris_lib/version.rb'
 include ChrisLib
 msg = "Build v#{ChrisLib::VERSION} of chris_lib"
-puts msg
+
 `gem build chris_lib`
 `git add .`
-`git commit -m #{msg}`
-puts "Build v#{ChrisLib::VERSION} of chris_lib"
+`git commit -m "#{msg}"`
 `git push origin master`
+puts msg
 `gem push chris_lib-#{ChrisLib::VERSION}.gem`
 puts "pushed v#{ChrisLib::VERSION} to rubygems and github"
 `git tag v#{ChrisLib::VERSION} -m msg`
