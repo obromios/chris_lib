68769d4827547b88094c533a5d642a70d663be81
obromios <obromios@gmail.com>
2022-03-03 11:22:21 +1100

    [0;32;49mBuild v2.0.8 of chris_lib[0m

--- script/push_gem.rb
+++ script/push_gem.rb
@@ -10,7 +10,7 @@ puts msg
 `git add .`
 `git commit -m "#{msg}"`
 puts "Pushing to github - may need personal access token".colorize(:yellow)
-puts "Find it at Evernote/calenders or github/settings/developer"
+puts "Find in Evernote or create at github/settings/developer"
 `git push origin master`
 puts 'Push to rubygems, need to enter OTP from Google Authenticator'.colorize(:yellow)
 system "gem push chris_lib-#{ChrisLib::VERSION}.gem"
