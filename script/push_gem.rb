bbd1aa861d11ccd7184a935054d9792cab85b2fe
obromios <obromios@gmail.com>
2022-03-03 11:20:57 +1100

    Improve documentation for push_gem

--- script/push_gem.rb
+++ script/push_gem.rb
@@ -1,15 +1,18 @@
 #!/usr/bin/env ruby
 require '/Users/Chris/Sites/chris_lib/lib/chris_lib/version.rb'
 require './lib/chris_lib/shell_methods.rb'
+require 'colorize'
 include ChrisLib
 include ShellMethods
-msg = "Build v#{ChrisLib::VERSION} of chris_lib"
+msg = "Build v#{ChrisLib::VERSION} of chris_lib".colorize(:green)
 puts msg
 `gem build chris_lib`
 `git add .`
 `git commit -m "#{msg}"`
+puts "Pushing to github - may need personal access token".colorize(:yellow)
+puts "Find it at Evernote/calenders or github/settings/developer"
 `git push origin master`
-puts 'Push to rubygems, need to enter OTP from Google Authenticator'
+puts 'Push to rubygems, need to enter OTP from Google Authenticator'.colorize(:yellow)
 system "gem push chris_lib-#{ChrisLib::VERSION}.gem"
 puts "pushed v#{ChrisLib::VERSION} to rubygems and github"
 `git tag v#{ChrisLib::VERSION} -m msg`
