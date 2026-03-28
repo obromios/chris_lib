eaf7ba17e10aabf3b68575b9fdb96014421d4ad0
obromios <obromios@gmail.com>
2026-03-28 16:29:08 +1100

    Fix gem release script

--- script/push_gem.rb
+++ script/push_gem.rb
@@ -1,27 +1,44 @@
 #!/usr/bin/env ruby
-require '/Users/Chris/Sites/chris_lib/lib/chris_lib/version.rb'
-require './lib/chris_lib/shell_methods.rb'
+# frozen_string_literal: true
+
+require 'bundler/setup'
 require 'colorize'
-include ChrisLib
-include ShellMethods
-msg = "Build v#{ChrisLib::VERSION} of chris_lib".colorize(:green)
-puts msg
-puts 'Assuming all tests passed'.colorize(:yellow)
-puts 'Assuming you have run bundle exec yard doc'.colorize(:yellow)
-puts "Assuming version is higher than deployed version".colorize(:yellow)
-puts "press any key to continue or ctrl-c to abort".colorize(:red)
-$stdin.getc
-`gem build chris_lib`.colorize(:yellow)
-`git add .`
-`git commit -m "#{msg}"`
-puts "Pushing to github - may need personal access token".colorize(:yellow)
-puts "create at github/settings/developer".colorize(:yellow)
-`git push origin master`
-puts 'Push to rubygems, need to enter OTP from Google Authenticator'.colorize(:yellow)
-system "gem push chris_lib-#{ChrisLib::VERSION}.gem"
-puts "pushed v#{ChrisLib::VERSION} to rubygems and github"
-`git tag v#{ChrisLib::VERSION} -m msg`
-`git push origin v#{ChrisLib::VERSION}`
-puts "pushed v#{ChrisLib::VERSION} tag"
-git_sha = `git log --pretty=format:'%h' -n 1`
-puts "Bump version, remove old .ruby-version files, update changelog with #{git_sha} #{time_hash}"
+require 'pathname'
+
+ROOT = Pathname(__dir__).join('..').expand_path
+$LOAD_PATH.unshift(ROOT.join('lib').to_s)
+
+require 'chris_lib/version'
+
+Dir.chdir(ROOT)
+
+def run!(*command)
+  success = system(*command)
+  return if success
+
+  abort("Command failed: #{command.join(' ')}")
+end
+
+version = ChrisLib::VERSION
+gem_file = "chris_lib-#{version}.gem"
+tag_name = "v#{version}"
+
+puts "Preparing #{gem_file}".colorize(:green)
+puts 'This script assumes tests already passed and master is already pushed.'.colorize(:yellow)
+puts 'RubyGems will prompt for OTP if MFA is enabled.'.colorize(:yellow)
+puts 'Press enter to continue or ctrl-c to abort.'.colorize(:red)
+$stdin.gets
+
+run!('gem', 'build', 'chris_lib.gemspec')
+run!('gem', 'push', gem_file)
+
+existing_tags = `git tag --list #{tag_name}`.lines.map(&:strip)
+if existing_tags.include?(tag_name)
+  puts "#{tag_name} already exists locally, skipping tag creation.".colorize(:yellow)
+else
+  run!('git', 'tag', tag_name, '-m', tag_name)
+end
+
+run!('git', 'push', 'origin', tag_name)
+
+puts "Published #{gem_file} and pushed #{tag_name}".colorize(:green)
